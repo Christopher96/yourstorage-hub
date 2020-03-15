649544248aeb0036abf307764b6cf7c458910448
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-03-15 17:43:49 +0100

    first commit

--- clientB.js
+++
@@ -1,9 +0,0 @@
-
-
-var remote = require('net').createConnection({host : "localhost", port : 34974},function () {
-  console.log('> (clientB) connected to clientA!');
-
-  c.on('data', function (data) {
-    console.log(data.toString());
-  });
-});
