aa135a338cf3296548df132b3e76716d5ae8ed1d
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-03-16 21:09:38 +0100

    hello

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const port = process.env.PORT || 9999;
 
 const server = require('net').createServer(function (socket) {
-  console.log('> Connect to this public endpoint with clientB:', socket.remoteAddress + ':' + socket.remotePort);
-}).listen(port, function (err) {
-  if(err) return console.log(err);
-  console.log('> (server) listening on:', server.address().address + ':' + server.address().port);
+    console.log('> Connect to this public endpoint with clientB:', socket.remoteAddress + ':' + socket.remotePort);
 
-  server.on('data', function (data) {
-    console.log(data.toString());
-  });
+    socket.on('data', function (data) {
+        console.log(data.toString());
+    });
+    socket.write("hello client");
+})
+
+server.listen(port, function (err) {
+    if(err) return console.log(err);
+    console.log('> (server) listening on:', server.address().address + ':' + server.address().port);
+
 });
+
+server.on('error', (err) => {
+    throw err;
+});
+
