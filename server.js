0b178f3a1d7a30e2de7acebbde0a7406984d6f3c
Christopher Gauffin <christopher.gauffin@gmail.com>
2020-03-16 20:41:27 +0100

    data

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ const server = require('net').createServer(function (socket) {
   console.log('> Connect to this public endpoint with clientB:', socket.remoteAddress + ':' + socket.remotePort);
 }).listen(port, function (err) {
   if(err) return console.log(err);
-  console.log('> (server) listening on:', server.address().address + ':' + server.address().port)
+  console.log('> (server) listening on:', server.address().address + ':' + server.address().port);
+
+  server.on('data', function (data) {
+    console.log(data.toString());
+  });
 });
